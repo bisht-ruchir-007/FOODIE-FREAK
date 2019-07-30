0ff152bb61dd9703a4ed7dc33f27be351b8ccf9e
bisht-ruchir-007 <32924441+bisht-ruchir-007@users.noreply.github.com>
2019-07-30 18:54:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React,{useEffect, useState} from 'react';
+import './App.css';
+import Recipe from './components/Recipe';
+import Footer from './components/Footer';
+import Ruchir from './components/Ruchir';
+
+const App = () =>{
+  const APP_ID = '22a49a6b';
+  const APP_KEY = '05e8bae24ef1e307ae707fe8b306b3e2';
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('')
+  const [query, setquery] = useState('');
+
+  useEffect(()=>{
+    getRecipes()
+  },[query]); 
+
+  const getRecipes = async()=>{
+     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+     const data = await response.json();
+     setRecipes(data.hits);
+     console.log(data);
+    };
+
+  const updateSearch = e =>{
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e =>{
+    e.preventDefault();
+    setquery(search);
+    setSearch('');
+  }
+
+
+  return (
+    <div className="App">
+      <p className="heading">THE-FOODIE-FREAK</p>
+      <Ruchir/>
+      <p className="t2">Browse the your food recipe from a collection of trusted comfort food recipes! </p>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} placeholder="ENTER A DISH eg: Chicken"/>
+        <button  className="search-button mx-2">
+          <strong>SEARCH</strong>
+        </button>
+      </form>
+      <p className="t1">RESULTS</p>
+      <div className="recipes">  
+        {recipes.map(recipe =>(
+          <Recipe 
+            key={recipe.recipe.label}
+            title={recipe.recipe.label} 
+            calories={recipe.recipe.calories} 
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+            dietLabels={recipe.recipe.dietLabels}
+            totalWeight={recipe.recipe.totalWeight}
+          />
+        ))
+        }
+        <Footer/>
+      </div>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = ()=>{
+    return (
+        <div class="card">
+        <div class="card-header">
+          By RUCHIR BISHT 
+        </div>
+        <div class="card-body">
+          <blockquote class="blockquote mb-0">
+            <p>THANKS TO EDAMAM : https://www.edamam.com/  </p>
+            <footer class="blockquote-footer">The Nutrition Analysis API and Database uses Natural Language Processing and semantically structured data.</footer>
+          </blockquote>
+        </div>
+      </div>
+    )
+}
+
+export default Footer;
